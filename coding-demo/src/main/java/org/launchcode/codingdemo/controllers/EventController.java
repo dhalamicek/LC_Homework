31ccf0f28bc4b7bba4844a55b2d594b29138a217
Debbie Niemann <debbieniemann@yahoo.com>
2022-11-30 18:56:54 -0600

    thymeleaf forms and fragments

--- coding-demo/src/main/java/org/launchcode/codingdemo/controllers/EventController.java
+++ coding-demo/src/main/java/org/launchcode/codingdemo/controllers/EventController.java
@@ -3,7 +3,9 @@ package org.launchcode.codingdemo.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -11,21 +13,23 @@ import java.util.List;
 @Controller
 @RequestMapping("events")
 public class EventController {
-
+    private static List<String> events = new ArrayList<>();
     @GetMapping
     public String displayAllEvents(Model model){
-        List<String> events = new ArrayList<>();
-        events.add("Code With Pride");
-        events.add("Strange Loop");
-        events.add("Apple WWDC");
-        events.add("SpringOne Platform");
-        model.addAttribute ("events", events);
-        return "events/index";
+        model.addAttribute("events", events);
+          return "events/index";
     }
 
-    //lives at /events
+    //lives at /events/create
     @GetMapping("create")
     public String renderCreateEventForm(){
         return "events/create";
+    }
+
+    //lives at /events/create
+    @PostMapping("create")
+    public String processCreateEventForm(@RequestParam String eventName){
+        events.add(eventName);
+        return "redirect:";
     }
 }
