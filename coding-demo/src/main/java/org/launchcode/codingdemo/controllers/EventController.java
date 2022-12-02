5146db3991f8d6bec12570a31472ea3d8069c71a
Debbie Niemann <debbieniemann@yahoo.com>
2022-12-02 17:10:29 -0600

    Display Validation Error Messages for a Field

--- coding-demo/src/main/java/org/launchcode/codingdemo/controllers/EventController.java
+++ coding-demo/src/main/java/org/launchcode/codingdemo/controllers/EventController.java
@@ -30,6 +30,7 @@ public class EventController {
     @GetMapping("create")
     public String displayCreateEventForm(Model model) {
         model.addAttribute("title", "Create Event");
+        model.addAttribute(new Event());
         return "events/create";
     }
 
@@ -37,7 +38,7 @@ public class EventController {
     public String processCreateEventForm(@ModelAttribute @Valid Event newEvent, Errors errors, Model model) {
         if(errors.hasErrors()){
             model.addAttribute("title", "Create Event");
-            model.addAttribute("errorMsg", "Bad data");
+
             return "events/create";
         }
         EventData.add(newEvent);
--- coding-demo/src/main/java/org/launchcode/codingdemo/models/Event.java
+++ coding-demo/src/main/java/org/launchcode/codingdemo/models/Event.java
@@ -14,14 +14,14 @@ public class Event {
     private int id;
     private static int nextId = 1;
 
- @NotBlank
+ @NotBlank(message = "Name is a required field.")
     @Size(min = 3, max = 50, message = "Name must be between 3 and 50 characters.")
     private String name;
     @Size(max = 500, message = "Description too long.")
     private String description;
 
 
-   @NotBlank
+   @NotBlank(message = "Email is a required field.")
    @Email(message = "Invalid email. Try again.")
     private String contactEmail;
 
@@ -32,6 +32,7 @@ public class Event {
         this.id = nextId;
         nextId++;
     }
+    public Event(){};
 
     public String getName() {
         return name;
